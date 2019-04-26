bbb337d6dff9fce4685020686c746e939e00b9a2
macnaforce <marcianosordam@gmail.com>
2019-04-26 13:52:19 +0200

    update plus screenshots

---
+++ miniJam/Assets/boatControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class boatControl : MonoBehaviour
+{
+    public float speed = 1f;
+
+    Transform target;
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        transform.position = transform.position + (movement*speed) * Time.deltaTime;
+
+        Vector3 targetDir = movement;
+        float step = speed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+
+    }
+
+}
