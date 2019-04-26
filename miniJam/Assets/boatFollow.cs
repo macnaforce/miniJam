f25f4e2ac7e7f48eb007c533b2f5573fa9c46519
macnaforce <marcianosordam@gmail.com>
2019-04-26 16:07:22 +0200

    miniJam Done

--- miniJam/Assets/boatFollow.cs
+++ miniJam/Assets/boatFollow.cs
@@ -3,11 +3,10 @@
 public class boatFollow : MonoBehaviour
 {
     public GameObject boat;
-    public float hight;
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(boat.transform.position.x, hight, boat.transform.position.z);
+        Vector3 newPos = new Vector3(boat.transform.position.x, transform.position.y, boat.transform.position.z);
         transform.position = newPos;
 
     }
---
+++ miniJam/Assets/cameraZoom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class cameraZoom : MonoBehaviour
+{
+    public GameObject vision;
+    public float height;
+    public float normalHeight;
+    private void OnTriggerEnter(Collider other)
+    {
+        vision.transform.position = new Vector3(vision.transform.position.x, height, vision.transform.position.z);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            vision.transform.position = new Vector3(vision.transform.position.x, normalHeight, vision.transform.position.z);
+    }
+
+
+}
