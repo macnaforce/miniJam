ac33adc612a39e947af69897964218ead643d25d
macnaforce <marcianosordam@gmail.com>
2019-04-26 14:34:36 +0200

    update

---
+++ miniJam/Assets/boatFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class boatFollow : MonoBehaviour
+{
+    public GameObject boat;
+    public float hight;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(boat.transform.position.x, hight, boat.transform.position.z);
+        transform.position = newPos;
+
+    }
+}
