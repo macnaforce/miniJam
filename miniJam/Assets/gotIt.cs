e60e98c40b8f33f59ca79f9ef21c4e3af52a6114
macnaforce <marcianosordam@gmail.com>
2019-04-26 15:15:40 +0200

    update

---
+++ miniJam/Assets/gotIt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class gotIt : MonoBehaviour
+{
+    bool inRange = false;
+    bool follow = false;
+    public GameObject boat;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        inRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inRange = false;
+    }
+    void Update()
+    {
+        //Detect when the E arrow key is pressed down
+        if ( Input.GetKeyDown(KeyCode.E) && inRange == true)
+            follow = true;
+
+        if (follow == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, boat.transform.position, 10f);
+        }
+    }
+}
